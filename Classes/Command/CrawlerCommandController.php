e8e322227d9c35d143169cd9ae97dbe848f11f26
Dominic Garms <djgarms@gmail.com>
2013-06-20 15:32:12 +0800

    [TASK] Scheduler task for re-crawling website based on sitemap.xml

---
+++ Classes/Command/CrawlerCommandController.php
@@ -0,0 +1,104 @@
+<?php
+namespace DMF\DdGooglesitemapDmf\Command;
+
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManager;
+use TYPO3\CMS\Extbase\Configuration\ConfigurationManagerInterface;
+use TYPO3\CMS\Extbase\Mvc\Controller\CommandController;
+
+/**
+ * Class CrawlerCommandController
+ * @package DMF\Intranet\Command
+ */
+class CrawlerCommandController extends CommandController {
+
+	/**
+	 * This function crawls generated Sitemap.xml from dd_googlesitemap and re-crawles
+	 * the whole website links found in it.
+	 *
+	 */
+	public function crawlXmlCommand() {
+
+		/** @var ConfigurationManager $configurationManager */
+		$configurationManager = GeneralUtility::makeInstance('TYPO3\\CMS\\Extbase\\Configuration\\ConfigurationManager');
+		$settings             = $configurationManager->getConfiguration(ConfigurationManagerInterface::CONFIGURATION_TYPE_FULL_TYPOSCRIPT);
+
+		$xmlUrls = $settings['plugin.']['dd_googlesitemap_dmf.']['crawler.'];
+		if (!is_array($xmlUrls)) {
+			return;
+		}
+		foreach ($xmlUrls as $httpUrl) {
+			$contentXml = $this->get_url_contents($httpUrl);
+
+			$generatedArrayOfXml = GeneralUtility::xml2tree($contentXml);
+			if ($urls = $generatedArrayOfXml['urlset'][0]['ch']['url']) {
+
+				$pathToLogFile = PATH_site . 'uploads/tx_ddgooglesitemap_dmf/';
+				if (!is_dir($pathToLogFile)) {
+					// create folder if not exist
+					$makeFolder = 'mkdir ' . $pathToLogFile;
+					exec("$makeFolder");
+				}
+
+				// clears all cache tables
+				$this->clearAllCaches();
+
+				// remove all temp files
+				$clearTypo3Temp = 'rm -rf ' . PATH_site . 'typo3temp/*';
+				exec("$clearTypo3Temp");
+
+				foreach ($urls as $url) {
+
+					// wget all sitemap urls
+					$http       = $url['ch']['loc'][0]['values'][0];
+					$wgetString = 'cd ' . $pathToLogFile . ' && rm wgetLog.txt && wget --no-cache --delete-after -a ' . $pathToLogFile . 'wgetLog.txt ' . $http;
+					exec("$wgetString");
+				}
+			}
+		}
+	}
+
+	/**
+	 * Clear all table caches
+	 *
+	 * @throws \RuntimeException
+	 */
+	protected function clearAllCaches() {
+		// Clear all caching framework caches
+		$GLOBALS['typo3CacheManager']->flushCaches();
+		if (\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::isLoaded('cms')) {
+			$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery('cache_treelist');
+		}
+		// Clearing additional cache tables:
+		if (is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearAllCache_additionalTables'])) {
+			foreach ($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['t3lib/class.t3lib_tcemain.php']['clearAllCache_additionalTables'] as $tableName) {
+				if (!preg_match('/[^[:alnum:]_]/', $tableName) && substr($tableName, -5) === 'cache') {
+					$GLOBALS['TYPO3_DB']->exec_TRUNCATEquery($tableName);
+				} else {
+					throw new \RuntimeException('TYPO3 Fatal Error: Trying to flush table "' . $tableName . '" with "Clear All Cache"', 1270853922);
+				}
+			}
+		}
+	}
+
+	/**
+	 * Get URL from http source
+	 *
+	 * @param $url
+	 *
+	 * @return mixed
+	 */
+	protected function get_url_contents($url) {
+		$crl     = curl_init();
+		$timeout = 5;
+		curl_setopt($crl, CURLOPT_URL, $url);
+		curl_setopt($crl, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt($crl, CURLOPT_CONNECTTIMEOUT, $timeout);
+		$ret = curl_exec($crl);
+		curl_close($crl);
+
+		return $ret;
+	}
+}
+
+?>
--- ext_localconf.php
+++ ext_localconf.php
@@ -5,5 +5,6 @@ if (!defined('TYPO3_MODE')) {
 }
 
 $GLOBALS['TYPO3_CONF_VARS']['EXTCONF']['dd_googlesitemap']['sitemap']['dmf'] = 'tx_ddgooglesitemap_dmf->main';
+$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['extbase']['commandControllers'][] = 'DMF\\DdGooglesitemapDmf\\Command\\CrawlerCommandController';
 
 ?>
