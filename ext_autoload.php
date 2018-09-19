f299870246cd7c1c10d9fe3affbaa0d0a36cf3ff
Philip Liebel <philip.liebel@cabag.ch>
2018-09-19 16:08:49 +0200

    Make extension compatible with TYPO3 8.7

--- ext_autoload.php
+++
@@ -1,5 +0,0 @@
-<?php
-$extpath = \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::extPath('dd_googlesitemap_dmf');
-return array(
-	'tx_ddgooglesitemap_dmf' => $extpath . 'class.tx_ddgooglesitemap_dmf.php',
-);
